bfa0996c4ff1653bb252ba7132278bb33b2673b9
Dylan On <dylanon@users.noreply.github.com>
2018-01-06 19:08:18 -0500

    Control the transaction input

--- dev/scripts/components/addTransaction.js
+++ dev/scripts/components/addTransaction.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
 export default class AddTransaction extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userString: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            userString: e.target.value
+        });
+    }
+
     render() {
         return (
             <form className="add-transaction__form">
                 <label htmlFor="add-transaction__input">Add Transaction</label>
-                <input type="text" id="add-transaction__input"/>
+                <input type="text" id="add-transaction__input" onChange={this.handleChange} value={this.state.userString} />
                 <input type="submit" value="Add"/>
+                <p>{this.state.userString}</p>
             </form>
         )
     }
