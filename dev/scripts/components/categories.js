9467273f6677f7b1f9cd19f6f377354c01203b7d
Dylan On <dylanon@users.noreply.github.com>
2018-01-12 22:40:18 -0500

    Add some controls to the categories list

--- dev/scripts/components/categories.js
+++ dev/scripts/components/categories.js
@@ -5,8 +5,21 @@ export default class Categories extends React.Component {
         const categoriesArray = this.props.categories;
         return (
             <ul>
+                <li>
+                    <a href="#">View all</a>
+                </li>
                 {categoriesArray.map(category => {
-                    return <li key={category.key}>#{category.category}</li>
+                    return (
+                        <li key={category.key}>
+                            <div>
+                                <a href="#">#{category.category}</a>
+                            </div>
+                            <div>
+                                <a href="#">Set budget</a>
+                            </div>
+                            {/* <a href="#">Remove</a> */}
+                        </li>
+                    )
                 })}
             </ul>
         )
