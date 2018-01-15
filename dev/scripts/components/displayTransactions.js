6ae897bf2dac55faf51f07bbb1643a27000e0828
Dylan On <dylanon@users.noreply.github.com>
2018-01-14 21:23:10 -0500

    Provide instructions when no transactions to display

--- dev/scripts/components/displayTransactions.js
+++ dev/scripts/components/displayTransactions.js
@@ -20,20 +20,22 @@ export default class DisplayTransactions extends React.Component {
 
     render() {
         const transactionsArray = Array.from(this.props.transactions);
-        // Sort transactions reverse chronologically
-        transactionsArray.sort((a , b) => {
-            const momentA = moment(a.date, 'YYYY-MM-DD');
-            const momentB = moment(b.date, 'YYYY-MM-DD');
-            if (momentB.isBefore(momentA, 'day')) {
-                return -1;
-            } else {
-                return 1;
-            }
-        });
+        let content;
 
-        return (
-            <section className="display-transactions">
-                <h2>Transactions</h2>
+        if (transactionsArray.length > 0) {
+            // If there are transactions to display, sort and display
+            // Sort transactions reverse chronologically
+            transactionsArray.sort((a , b) => {
+                const momentA = moment(a.date, 'YYYY-MM-DD');
+                const momentB = moment(b.date, 'YYYY-MM-DD');
+                if (momentB.isBefore(momentA, 'day')) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            });
+            // Build the content
+            content = (
                 <ul className="display-transactions__list">
                     {transactionsArray.map(transaction => {
                         return (
@@ -49,6 +51,40 @@ export default class DisplayTransactions extends React.Component {
                         )
                     })}
                 </ul>
+            )
+        } else {
+            // If there are no transactions to display, show some instructions
+            content = (
+                <div className="no-transactions">
+                    <p className="no-transactions__lead">No transactions to show 🤑</p>
+                    <p>Create one by typing one or more details into the box below:</p>
+                    <ul className="fa-ul">
+                        <li>
+                            <span className="fa-li"><i className="fas fa-calendar"></i></span>
+                            <span className="no-transactions__emphasize">Date</span> (<code>jan21</code> or <code>Jan 21</code> will both work)
+                        </li>
+                        <li>
+                            <span className="fa-li"><i className="fas fa-dollar-sign"></i></span>
+                            <span className="no-transactions__emphasize">Amount</span> (<code>$5.00</code>, <code>$5</code>, or even just <code>5</code>)
+                        </li>
+                        <li>
+                            <span className="fa-li"><i className="fas fa-font"></i></span>
+                            <span className="no-transactions__emphasize">Description</span> (Some text like <code>Chicken soup and apple pie</code>)
+                        </li>
+                        <li>
+                            <span className="fa-li"><i className="fas fa-hashtag"></i></span>
+                            <span className="no-transactions__emphasize">Category</span> (<code>#fun</code>, <code>#groceries</code>, <code>#dogstuff</code>)
+                        </li>
+                    </ul>
+                    <p>Don't worry - I'll create some default values for info you don't provide!</p>
+                </div>
+            )
+        }
+
+        return (
+            <section className="display-transactions">
+                <h2>Transactions</h2>
+                {content}
             </section>
         )
     }
